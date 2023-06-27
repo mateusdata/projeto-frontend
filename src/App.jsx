0ef38e2d859a974e82c4d0b3ad1b4a7b56247f7b
mateusdata <mateuspele2015@gmail.com>
2023-06-27 00:13:29 -0300

    add new folder

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from 'react';
+import axios from "axios";
+
+function App() {
+  const [users, setUser] = useState([]);
+  const x  = {
+    preco:10, cor:"blue",
+    preco:100, cor:"blue",
+    preco:20, cor:"red",
+  }
+useEffect(()=>{
+  axios.get("http://localhost:3001/").then((respose)=>{
+    console.log(respose);
+    setUser(respose?.data);
+    setUser(respose?.data);
+  })
+},[])
+
+  return (
+    <div className="bg-white flex items-center justify-center flex-col ">
+      <h1 className="text-2xl">
+       Projeto academia
+      </h1>
+      {users.map((item)=>(
+        <div key={item.id}> 
+          <p>{item?.id}</p>
+          <p>{item?.name}</p>
+          <p>{item?.email}</p>
+          <p>{item?.password}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default App;
