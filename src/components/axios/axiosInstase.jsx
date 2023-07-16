4db08cd45acdd3a133c7e9b9818fd99aa6e63262
mateusdata <mateuspele2015@gmail.com>
2023-07-16 00:17:32 -0300

    update index.js

--- src/components/axios/axiosInstase.jsx
+++ src/components/axios/axiosInstase.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 const axiosInstase = axios.create({
-    baseURL:"http://localhost:3003/"
+    baseURL:"http://localhost:3003"
 })
 export default axiosInstase;
--- src/components/form/addUser.jsx
+++ src/components/form/addUser.jsx
@@ -1,24 +1,42 @@
-import React from 'react';
-
-
+import React, { useState } from 'react';
+import axiosInstase from '../axios/axiosInstase';
 
 const Formulario = () => {
+  const [user, setUser] = useState({ nome: '', email: '', password: '' });
 
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUser((prevState) => ({ ...prevState, [name]: value }));
+   
+    console.log(user);
+  };
 
-
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(user.email && user.password && user.nome){
+      axiosInstase.put("/admin/student").then((response)=>{
+        console.log(response);
+        return;
+      }).catch((erro)=>console.log("erro"));
+    
+    }
+    alert("Não é posivel criar usuarios");
+  };
 
   return (
     <div className="container mx-auto px-4 py-8">
       <h1 className="text-2xl font-bold mb-4">Formulário</h1>
-      <form className="max-w-xl mx-auto">
-      
+      <form className="max-w-xl mx-auto" onSubmit={handleSubmit}>
         <div className="mb-4">
           <label htmlFor="nome" className="block text-gray-700 font-medium mb-2">
             NOME
           </label>
           <input
+            name="nome"
+            onChange={handleChange}
             type="text"
             id="nome"
+            value={user.nome}
             className="border border-purple-200 rounded-lg px-3 py-2 w-full focus:outline-none focus:ring-2 focus:ring-purple-400"
           />
         </div>
@@ -27,25 +45,29 @@ const Formulario = () => {
             EMAIL
           </label>
           <input
+            name="email"
+            onChange={handleChange}
             type="email"
             id="email"
+            value={user.email}
             className="border border-purple-200 rounded-lg px-3 py-2 w-full focus:outline-none focus:ring-2 focus:ring-purple-400"
           />
         </div>
         <div className="mb-4">
-          <label htmlFor="tipo-treino" className="block text-gray-700 font-medium mb-2">
-            SENHA
+          <label htmlFor="password" className="block text-gray-700 font-medium mb-2">
+            password
           </label>
           <input
-            type="text"
-            id="tipo-treino"
+            name="password"
+            onChange={handleChange}
+            type="password"
+            id="password"
+            value={user.password}
             className="border border-purple-200 rounded-lg px-3 py-2 w-full focus:outline-none focus:ring-2 focus:ring-purple-400"
           />
         </div>
-      
         <div className="flex justify-center">
           <button
-
             type="submit"
             className="bg-purple-300 text-white rounded-lg px-4 py-2 font-medium hover:bg-purple-400 focus:outline-none focus:bg-purple-400"
           >
